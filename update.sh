956f49abfbf8c50ca6035b42aed6d29411ec7e86
Andreas Hernandez <andreashdez@gmail.com>
2017-04-01 21:19:28 +0100

    update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-cp -r ~/.bin/ ./.bin/
 cp -r ~/.config/bspwm/ ~/.config/compton/ ~/.config/i3/ ~/.config/polybar/ ~/.config/rofi/ ~/.config/sxhkd/ ~/.config/termite/ ./.config/
 cp ~/.config/nvim/init.vim ./.config/nvim/
 cp -r ~/.weechat/buffers.conf ~/.weechat/weechat.conf ./.weechat/
-cp -r ~/.xinitrc ~/.Xresources ~/.zshrc ./
+cp -r ~/.bin/ ~/.xinitrc ~/.Xresources ~/.zshrc ./
