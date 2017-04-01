e1a43025e353e3cb6680ff5479bfa89cd660362e
Andreas Hernandez <andreashdez@gmail.com>
2017-04-01 21:13:13 +0100

    update config

--- update.sh
+++ update.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-cp -r ~/.config/compton ~/.config/i3 ~/.config/polybar ~/.config/rofi ~/.config/termite ./.config/
+cp -r ~/.bin/ ./.bin/
+cp -r ~/.config/bspwm/ ~/.config/compton/ ~/.config/i3/ ~/.config/polybar/ ~/.config/rofi/ ~/.config/sxhkd/ ~/.config/termite/ ./.config/
 cp ~/.config/nvim/init.vim ./.config/nvim/
-cp ~/.config/cmus/gruvbox-dark.theme ./.config/cmus/
-cp ~/.config/bspwm/bspwmrc ./.config/bspwm/
-cp ~/.config/sxhkd/sxhkdrc ./.config/sxhkd/
 cp -r ~/.weechat/buffers.conf ~/.weechat/weechat.conf ./.weechat/
 cp -r ~/.xinitrc ~/.Xresources ~/.zshrc ./
