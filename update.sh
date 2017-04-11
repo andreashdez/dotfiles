edf46242f8aa657bd7dbceb2873cc7cd6fc40f02
Andreas Hernandez <andreashdez@gmail.com>
2017-04-11 19:09:31 +0100

    + mpd, ncmpcpp

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-cp -r ~/.config/bspwm/ ~/.config/compton/ ~/.config/i3/ ~/.config/polybar/ ~/.config/rofi/ ~/.config/sxhkd/ ~/.config/termite/ ./.config/
+cp -r ~/.config/bspwm/ ~/.config/compton/ ~/.config/ncmpcpp/ ~/.config/polybar/ ~/.config/rofi/ ~/.config/sxhkd/ ~/.config/termite/ ./.config/
 cp ~/.config/nvim/init.vim ./.config/nvim/
+cp ~/.config/mpd/mpd.conf ./.config/mpd/
 cp -r ~/.weechat/buffers.conf ~/.weechat/weechat.conf ./.weechat/
 cp -r ~/.bin/ ~/.xinitrc ~/.Xresources ~/.zshrc ./
