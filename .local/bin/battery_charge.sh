626b4d723db30188ca08beb5113a13fb0784fcab
Andreas Hernández Hauser <andreashdez@gmail.com>
2020-04-04 12:58:41 +0200

    fix battery charge for multiple batteries

--- .local/bin/battery_charge.sh
+++ .local/bin/battery_charge.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-acpi --battery | awk -F ', ' '{ print match($3, /[^ ]/) ? $2 " (" $3 ")" : $2 }'
+acpi --battery \
+    | tail -n 1 \
+    | awk -F ', ' '{ print match($3, /[^ ]/) ? $2 " (" $3 ")" : $2 }'
 
