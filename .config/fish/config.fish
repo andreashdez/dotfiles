5423b2a1b3134f7842ddfeceeb53e4edf7151004
Andreas Hernández Hauser <andreashdez@gmail.com>
2021-08-08 21:01:36 +0200

    add latest changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,40 +3,23 @@
 
 set fish_greeting
 
-# set -U  fish_user_paths            /usr/local/opt/openssl/bin ~/bin $HOME/.asdf/shims
-# set -gx FZF_DEFAULT_OPTS           '--height=50% --min-height=15 --reverse'
-# set -gx FZF_DEFAULT_COMMAND        'rg --files --no-ignore-vcs --hidden'
-# set -gx FZF_CTRL_T_COMMAND         $FZF_DEFAULT_COMMAND
-# set -gx EVENT_NOKQUEUE             1
-# set -gx EDITOR                     nvim
-# set -gx HOMEBREW_FORCE_VENDOR_RUBY 1
-# set -gx GPG_TTY                    (tty)
-# set -gx ASDF_DIR                   /usr/local/opt/asdf
-
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
-if status --is-interactive
-    abbr --add --global l 'exa -la'
-    abbr --add --global ll 'exa -l'
-    abbr --add --global vi 'nvim'
-    abbr --add --global hh 'history | fzf'
-end
+abbr --add --global ll 'exa -l'
+abbr --add --global llr 'exa -lR'
+abbr --add --global lla 'exa -la'
+abbr --add --global vi 'nvim'
+abbr --add --global hh 'history | sk'
 
-# ## SOURCES
-# [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-# [ -f /usr/local/opt/asdf/asdf/asdf.fish ]; and source /usr/local/opt/asdf/asdf.fish
+# set --global --export --path PATH "$PATH:$HOME/.cache/cargo/bin:$HOME/.go/bin:$HOME/.local/bin"
 
-# ## GPG
-# gpg-agent --daemon --no-grab >/dev/null ^&1
+set --global fish_cursor_default block
+set --global fish_cursor_insert line
+set --global fish_cursor_visual underscore
 
-# ## TMUX
-# if status --is-interactive
-#     and command -s tmux >/dev/null
-#     and not set -q TMUX
-#     exec tmux new -A -s (whoami)
-# end
+set --global fish_color_command white --bold
+
+set --global tide_right_prompt_suffix ' '
 
 fish_vi_key_bindings
-
-starship init fish | source
 
