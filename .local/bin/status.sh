1fb21c732cb3d36f62663e38d3fff1d2c53d0d09
Andreas Hernández Hauser <andreashdez@gmail.com>
2023-05-13 20:36:57 +0200

    add adjusted configs

--- .local/bin/status.sh
+++ .local/bin/status.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-printf " %s       %s      %s      %s      %s   ┊  %s   " \
+printf "   %s         %s        %s        %s        %s   ┊  %s   " \
     "$(brillo)" \
     "$("$HOME/.local/bin/volume_status.sh")" \
     "$("$HOME/.local/bin/network.sh")" \
-    "$("$HOME/.local/bin/battery_charge.sh")" \
     "$("$HOME/.local/bin/disk_space.sh")" \
+    "$("$HOME/.local/bin/battery_charge.sh")" \
     "$(date +'%Y-%m-%d    %H:%M:%S')"
 
